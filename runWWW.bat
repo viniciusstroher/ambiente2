4cb5d0e59f1d70b23f91384a92ab5d60eb6f8452
unknown <viniciusferreirawk@gmail.com>
2018-04-03 20:03:39 -0300

    fix

--- runWWW.bat
+++ runWWW.bat
@@ -1,4 +1,5 @@
+docker build https://github.com/viniciusstroher/docker-apache-php56#container:docker
 docker run -d -p 127.0.0.1:8000:80 ^
     -v /c/Users/veni/Desktop/docker/ambiente2/www:/var/www ^
     -e PHP_ERROR_REPORTING="E_ALL & ~E_STRICT" ^
-    bylexus/apache-php56
+    viniciusstroher/docker-apache-php56
